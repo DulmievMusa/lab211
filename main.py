86465b2c86bd4551fbb9e1e99cd24fd8cf1f759b
Муса Дульмиев <musadulmiev@gmail.com>
2026-04-26 20:19:28 +0300

    table

--- main.py
+++ main.py
@@ -4,19 +4,9 @@ from math import pi
 
 d = 0.95
 L = 125 * (10 ** -3)
+beta = 40.7
 
-h_es, v_es, t_es = get_all_columns_from_file("data1.csv")
-q_es = [(v_es[i] * (10 ** -6)) / t_es[i] for i in range(len(v_es))]
-h_es = [h/1000 for h in h_es]
-
-k, b, sigma_k, sigma_b = linear_regression(h_es, q_es)
-
-print(f"vyaskost: {(pi * ((0.95 * 10 ** -3) ** 4) * 1000 * 9.81)/(8 * 125 * (10 ** -3) * k)}")
-
-
-print("k", k)
-paint_line_function(k, b, h_es, y_es=q_es)
-
-
-
-set_end()
+I_es, U_es, E_es = get_all_columns_from_file("data3.csv")
+print("eps, U, I, delta T, N, R_н")
+for i in range(4):
+    print(f"{E_es[i]}, {U_es[i]}, {I_es[i]}, {my_round(E_es[i]/beta, 2)}, {my_round((I_es[i] * U_es[i])/1000, 2)}, {my_round((U_es[i]*1000)/I_es[i], 2)}")
