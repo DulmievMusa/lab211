97dcd1b1f4944f1a7ba3fc827ae5fa434bcaab54
Муса Дульмиев <musadulmiev@gmail.com>
2026-04-30 12:34:37 +0300

    peredelka

--- graph_2.py
+++ graph_2.py
@@ -12,15 +12,17 @@ for i in range(4):
     dT_es.append(E_es[i]/beta)
     N_es.append((I_es[i] * U_es[i])/1000)
 
-k, b, sigma_k, sigma_b = linear_regression(N_es, dT_es)
+k, sigma_k = origin_linear_regression(N_es, dT_es)
 
 
 
 print("k", k)
 print("sigma_k", sigma_k)
 print("eps", (sigma_k/k) * 100)
-paint_line_function(k, b, N_es, y_es=dT_es)
+N_es.insert(0, 0)
+dT_es.insert(0, 0)
+paint_line_function(k, 0, N_es, y_es=dT_es, color_number=8, size=4)
 
 
 
-set_end(title="График изменения мощности от изменения температуры", y_label="$\Delta T, \\text{К}$", x_label="N, Вт")
+set_end(title="График изменения мощности от изменения температуры", y_label="$\Delta T, \\text{К}$", x_label="N, Вт", show_origin=True)
